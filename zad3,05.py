24716e33f47a0199102787cbee74fecf0ee11296
Valentina Tepavicharova <147132606+ValentinaTepavicharova@users.noreply.github.com>
2023-11-02 23:01:36 +0200

    Update zad3,05.py

--- zad3,05.py
+++ zad3,05.py
@@ -1,10 +1,13 @@
 from collections import deque
 food=int(input())
+#konstruktora , koyato priema list
 orders=deque([int(i) for i in input().split(" ")])
 print(max(orders))
+#ako hranata e poveche shte se izpulnqt poruchkite
 if sum(orders)<=food:
     print("Orders complete")
 else:
     while food > 0:
+#vadim wsyaka poruchka i kato ostane q printirame
         food-=orders.popleft
     print(f"Left orders:{orders} " )
